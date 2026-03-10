cdc479b7b32d314f5f4efb286a849f5a614c24f5
julian-fernandez <julian.ferval@gmail.com>
2026-03-10 13:40:08 -0300

    Implement carousel container and cards

--- src/app/components/image-carousel-section/image-carousel-section.component.ts
+++ src/app/components/image-carousel-section/image-carousel-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ImageCarouselCardComponent } from '../image-carousel-card/image-carousel-card.component';
 
 @Component({
   selector: 'app-image-carousel-section',
   standalone: true,
-  imports: [],
+  imports: [ImageCarouselCardComponent],
   templateUrl: './image-carousel-section.component.html',
   styleUrl: './image-carousel-section.component.scss'
 })
